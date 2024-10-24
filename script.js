a383ff6c6a49d6bebab06a1e2373ad42314a19c9
Jay <ihearthuskiez@gmail.com>
2024-10-23 21:00:37 -0600

    YEEEAAAAH ALL THATS LEFT IS LOCAL STORAGE

--- script.js
+++ script.js
@@ -8,9 +8,13 @@ class list {
         this.items.push(item);
     }
 
-    removeItem(index) {
+    /*removeItem(index) {
         // https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array-in-javascript
         this.items.splice(index, 1);
+    }*/
+
+    removeItem() {
+        this.items.pop();
     }
 
     consoleLog() {
@@ -35,46 +39,34 @@ travelers.addItem("Feldspar");
 travelers.addItem("Solanum");
 
 let listOfLists = [shoppingList, travelers];
+let chosenIndex = 0;
+renderListItems(0);
+renderListTitles();
 
 function renderListTitles() {
     let output = "";
     for (let i = 0; i < listOfLists.length; i++) {
         let listTitle = listOfLists[i].name;
         let thisList = listOfLists[i];
-        output += `<span class="listName"">${listTitle}</span>`;
+        // console.log(thisList);
+        output += `<span class="listName" onclick="renderListItems(${i})">${listTitle}</span>`;
+        //output += "<span class='listName' onclick='renderListItems(" + thisList + ")'>" + listTitle + "</span>";
     }
     document.getElementById("lists").innerHTML = output;
 }
 
-renderListItems(listOfLists[1]);
-
-renderListTitles();
-
-function renderListItems(list) {
+function renderListItems(listIndex) {
+    let list = listOfLists[listIndex];
+    chosenIndex = listIndex;
     let output = "";
     for (let i = 0; i < list.items.length; i++) {
         let addToOutput = list.items[i];
         output += `<div class="wholeListItem"><input type="checkbox"> <span class="listItem">${addToOutput}</span></div>`;
     }
     document.getElementById("showList").innerHTML = output;
-}
-
-function addItem() {
-    let item = document.getElementById("addItemInput").value;
-    if (!item) {
-        alert("PUT A THING IN THE TEXTBOX DON'T LEAVE IT EMPTY");
-        return;
-    }
-    document.getElementById("addItemInput").value = "";
-    let chosenList = travelers; //this can be changed later to make it work on different lists, but for now it's just the one.
-    chosenList.items.push(item);
-    renderListItems(chosenList);
-}
-
-function removeItem() {
-    let chosenList = travelers; //this can be changed later to make it work on different lists, but for now it's just the one.
-    chosenList.items.pop();
-    renderListItems(chosenList);
+    document.getElementById("listTitle").innerHTML = list.name;
+    // console.log(chosenIndex);
+    // console.log(listOfLists[chosenIndex]);
 }
 
 function makeList() {
@@ -93,3 +85,42 @@ function removeList() {
     listOfLists.pop();
     renderListTitles();
 }
+
+function addItem() {
+    // console.log(chosenIndex);
+    // console.log(listOfLists[chosenIndex]);
+
+    let item = document.getElementById("addItemInput").value;
+    if (!item) {
+        alert("PUT A THING IN THE TEXTBOX DON'T LEAVE IT EMPTY");
+        return;
+    }
+    document.getElementById("addItemInput").value = "";
+
+    listOfLists[chosenIndex].addItem(item);
+    renderListItems(chosenIndex);
+}
+
+function removeItem() {
+    listOfLists[chosenIndex].removeItem();
+    renderListItems(chosenIndex);
+}
+/*
+function addItem() {
+    let item = document.getElementById("addItemInput").value;
+    if (!item) {
+        alert("PUT A THING IN THE TEXTBOX DON'T LEAVE IT EMPTY");
+        return;
+    }
+    document.getElementById("addItemInput").value = "";
+    let chosenList = listOfLists[chosenIndex]; //this can be changed later to make it work on different lists, but for now it's just the one.
+    chosenList.addItem(item);
+    renderListItems(chosenList);
+}
+
+function removeItem() {
+    let chosenList = listOfLists[chosenIndex]; //this can be changed later to make it work on different lists, but for now it's just the one.
+    chosenList.removeItem();
+    renderListItems(chosenList);
+}
+*/
